9e6df5696a6a7315748380837db501fcf844832d
Tom ten Thij <git@tomtenthij.nl>
2015-03-10 23:37:02 +0000

    Adding some minor modes

--- lib/music/scale.rb
+++ lib/music/scale.rb
@@ -5,8 +5,22 @@ module Music
 
     SCALE_MAPPINGS = {
       major: :ionian,
-      harmonic_minor: :aeolian
+      natural_minor: :aeolian
     }
+
+    # lydian:           [0, 2, 4, 6, 7, 9, 11]
+    #   ^ raise 4th
+    # ionian:           [0, 2, 4, 5, 7, 9, 11]
+    #   v Lower 7th
+    # mixolydian:       [0, 2, 4, 5, 7, 9, 10]
+    #   v lower 3rd
+    # dorian:           [0, 2, 3, 5, 7, 9, 10]
+    #   v lower 6th
+    # aeolian:          [0, 2, 3, 5, 7, 8, 10]
+    #   v lower 2nd
+    # phrygian:         [0, 1, 3, 5, 7, 8, 10]
+    #   v lower 5th
+    # locrian:          [0, 1, 3, 5, 6, 8, 10]
 
     SCALE_MODES = {
       minor_pentatonic: [0, 3, 5, 7, 10],
@@ -17,7 +31,10 @@ module Music
       lydian:           [0, 2, 4, 6, 7, 9, 11], # wwwhwwh
       mixolydian:       [0, 2, 4, 5, 7, 9, 10], # wwhwwhw
       aeolian:          [0, 2, 3, 5, 7, 8, 10], # whwwhww
-      locrian:          [0, 1, 3, 5, 6, 8, 10]  # hwwhwww
+      locrian:          [0, 1, 3, 5, 6, 8, 10], # hwwhwww
+      harmonic_minor:   [0, 2, 3, 5, 7, 8, 11],
+      melodic_minor:    [0, 2, 3, 5, 7, 9, 11]
+
     }
 
     def initialize(args)
